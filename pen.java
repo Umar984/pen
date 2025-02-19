ef9f0b7950a9a87eef3afe70907021ee5025c490
Md Umar Faruk <mdumarfaruk793@gmail.com>
2025-02-19 20:25:25 +0530

    Add files via upload

---
+++ pen.java
@@ -0,0 +1,14 @@
+class myclass
+{
+int no-of-banch;
+String name;
+public static void main(String[] args)
+{
+myclass A1 = New myclass();
+A1.no-of-banch = 10;
+A1.name = "umar";
+System.out.println(A1.no-of-banch);
+System.out.println(A1.name);
+
+}
+}
